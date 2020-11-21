35f4e1e65d1fa16b83f9df54386538e4ce64469b
Hello1029384756 <65038538+Hello1029384756@users.noreply.github.com>
2020-11-20 19:29:07 -0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import LoginScreen from './screen/LoginScreen';
+
+export default function App () {
+
+  return (
+    <LoginScreen/>
+    );
+  }
